04207d3dfb5ceccc9cf25d2cd2315b22fd4b68a8
VNUS-Solution <vnussolution@gmail.com>
2017-12-20 17:57:23 -0800

    update cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
