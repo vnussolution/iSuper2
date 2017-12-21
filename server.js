e53d06807ed79d6d97454efe208de6034db38b74
VNUS-Solution <vnussolution@gmail.com>
2017-12-20 17:50:49 -0800

    add cors back

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.use(bodyParser.urlencoded({
   'extended': 'true'
 }));
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors());
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
